8f08b9ba4ce06241ad79f3800af6c5d83ac153ff
soufianeargane <soufianeargane800@gmail.com>
2023-12-13 15:02:25 +0100

    added pdf for single appartements with styling

--- appartement-backend/controllers/PaymentController.js
+++ appartement-backend/controllers/PaymentController.js
@@ -63,10 +63,13 @@ const getSinglePayment = async (req, res) => {
         const today = new Date();
         const month = today.getMonth() + 1;
         const year = today.getFullYear();
+
         // apartmentId
         const payment_id = req.params.id;
+
         // Fetch all apartments
         const allApartments = await AppartementModel.find();
+
         // Fetch paid apartments for the current month
         const payment = await PaymentModel.find({
             _id: payment_id,
@@ -75,9 +78,10 @@ const getSinglePayment = async (req, res) => {
             apartment: { $in: allApartments.map((apartment) => apartment._id) },
         }).populate("apartment");
 
-        if (!payment) {
+        if (!payment || payment.length === 0) {
             return res.status(404).send("Payment not found");
         }
+
         const apartment = payment[0].apartment;
 
         // Create a new PDF document
@@ -91,22 +95,64 @@ const getSinglePayment = async (req, res) => {
         );
 
         // Pipe the PDF to the response
-        doc.pipe(res);
+        doc.pipe(res, { end: true });
 
-        // Add content to the PDF
-        doc.fontSize(16).text("Facture Bill", { align: "center" }).moveDown();
-        doc.fontSize(12).text(`Owner: ${apartment.owner}`);
-        doc.text(`Building: ${apartment.building}`);
-        doc.text(`Apartment Number: ${apartment.number}`);
-        doc.text(`Status: ${apartment.status}`);
-        doc.text(`Amount: $${payment[0].amount}`);
+        generateHeader(doc);
+
+        doc.text(`Apartement Number:`, 50, 200)
+            .text(`${apartment.number}`, 200, 200)
+
+            .text("Apartment Building:", 50, 215)
+            .text(`${apartment.building}`, 200, 215)
+            .text("Date:", 50, 230)
+            .text(`${today.toLocaleDateString()}`, 200, 230)
+            .fontSize(15)
+            .font("Helvetica-Bold")
+            .text("Owner:", 400, 200, { align: "right" })
+            .text(`${apartment.owner}`, 550, 200, {
+                align: "right",
+            })
+            .text("Status:", 400, 225, { align: "right" })
+            .text(`${apartment.status}`, 550, 225, {
+                align: "right",
+            });
+
+        doc.moveDown();
+
+        generateTable(doc, payment[0]);
 
         // Finalize the PDF
         doc.end();
     } catch (error) {
         console.error(error);
+        res.status(500).json({ error: "Internal server error" });
     }
 };
+
+function generateHeader(doc) {
+    doc.image("logo.png", 50, 45, { width: 50 })
+        .fillColor("#444444")
+        .fontSize(20)
+        .text("SANDIC managment", 110, 57)
+        .fontSize(10)
+        .text("YouCode", 200, 65, { align: "right" })
+        .text("Youssoufia, Morocco", 200, 80, { align: "right" })
+        .moveDown();
+}
+
+function generateTable(doc, payment) {
+    const tableTop = 250;
+
+    doc.font("Helvetica-Bold");
+    doc.fontSize(12).text("Amount Paid", 50, tableTop);
+    doc.text("Taxes", 200, tableTop);
+    doc.text("Total", 300, tableTop);
+
+    doc.font("Helvetica");
+    doc.fontSize(12).text(`${payment.amount}`, 50, tableTop + 25);
+    doc.text("0", 200, tableTop + 25);
+    doc.text(`${payment.amount}`, 300, tableTop + 25);
+}
 
 module.exports = {
     createPayment,
